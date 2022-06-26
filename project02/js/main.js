6e0d978e1e7fe636e2b1889a643d96d56d7e2226
seo0033 <tkrhk9511@gmail.com>
2022-06-27 01:18:45 +0900

    20220627

--- project02/js/main.js
+++ project02/js/main.js
@@ -1,8 +1,15 @@
-$(function(){
+$(function () {
     $('.main_slider').slick({
-        arrows: false,
+        // arrows: false,
         pauseOnHover: false,
         pauseOnFocus: false,
     });
+
+    $('.main_arrows i:nth-child(1)').on('click', function () {
+        $('.main_slider').slick('slickPrev')
+    })
+    $('.main_arrows i:nth-child(2)').on('click', function () {
+        $('.main_slider').slick('slickNext')
+    })
 
 })//the end
